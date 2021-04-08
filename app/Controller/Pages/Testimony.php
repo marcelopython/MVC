6142dfd90c0b88575be72d8041ee50c0a841385a
marcelopython <msr88957@gmail.com>
2021-04-07 22:20:21 -0300

    Configuração para tela de depoimentos

---
+++ app/Controller/Pages/Testimony.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: msr
+ * Date: 07/04/21
+ * Time: 21:07
+ */
+
+namespace App\Controller\Pages;
+
+
+class Testimony
+{
+
+}
---
+++ app/Model/Entity/Testimony.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: msr
+ * Date: 07/04/21
+ * Time: 22:05
+ */
+
+namespace App\Model\Entity;
+
+
+class Testimony
+{
+
+}
